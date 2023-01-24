addd1eab50f5163fa1415a2aaadf60d02832b9d1
epetrich <epetrich@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2023-01-24 07:29:21 +0000

    * snap.php: don't misrecognize lines beginning with   numbers as failure counts
    git-svn-id: svn://svn.code.sf.net/p/sdcc/code/trunk@13816 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-web/snap.php
+++ sdcc-web/snap.php
@@ -203,9 +203,13 @@ function rt_failed($fname)
   if ($handle = fopen($fname, "r")) {
     while ($line = fgets($handle)) {
       # Summary for 'host': 0 failures, 4244 tests, 596 test cases, 0 bytes, 0 ticks
-      $failures = preg_replace('/^Summary for \'.+\':.* (\d+) failures, \d+ tests, \d+ test cases, \d+ bytes, \d+ ticks/',
-        '$1', $line);
-      if (($failures && $failures > 0) || preg_match('/Error/', $line) || preg_match('/invalid instructions/', $line))
+      if (preg_match('/^Summary/', $line) {
+        $failures = preg_replace('/^Summary for \'.+\':.* (\d+) failures, \d+ tests, \d+ test cases, \d+ bytes, \d+ ticks/',
+          '$1', $line);
+        if ($failures && $failures > 0)
+          return true;
+      }
+      if (preg_match('/Error/', $line) || preg_match('/invalid instructions/', $line))
         return true;
     }
   
