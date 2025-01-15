92947c6cc207676b70f0bc1858b585836a5bed4f
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2025-01-15 16:19:24 +0000

    Announce SDCC 4.5.0 RC2.
    git-svn-id: svn://svn.code.sf.net/p/sdcc/code/trunk@15226 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc-web/index.php
+++ sdcc-web/index.php
@@ -67,6 +67,11 @@
             
             <h2><a name="News"></a>News</h2> 
 
+            <p><i><b>January 15th, 2025: SDCC 4.5.0 RC2 released.</b></i></p>
+            <p>SDCC 4.5.0 Release Candidate 2 source, doc and binary packages for amd64 GNU/Linux, amd64 Windows, x86 Windows, and amd64 macOS are available in corresponding folders at:
+              <a href="http://sourceforge.net/projects/sdcc/files/">
+              http://sourceforge.net/projects/sdcc/files/</a>.</p>
+
             <p><i><b>January 3rd, 2025: SDCC 4.5.0 RC1 released.</b></i></p>
             <p>SDCC 4.5.0 Release Candidate 1 source, doc and binary packages for amd64 GNU/Linux, amd64 Windows, x86 Windows, and amd64 macOS are available in corresponding folders at:
               <a href="http://sourceforge.net/projects/sdcc/files/">
